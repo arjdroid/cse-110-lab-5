dda3a2597f07ddb0efd6297868a58ab671b40614
Arjun Singh <57623600+Arjdroid@users.noreply.github.com>
2026-05-02 09:27:43 +0000

    populate voice selector

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,19 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    const voiceSelect = document.querySelector('#voice-select');
+
+    function populateVoices() {
+        const voices = speechSynthesis.getVoices();
+        voices.forEach((voice) => {
+            const option = document.createElement('option');
+            option.value = voice.name;
+            option.textContent = `${voice.name} (${voice.lang})`;
+            voiceSelect.appendChild(option);
+        });
+        // great, works on Chromium but not FF... :(
+    }
+
+    populateVoices();
+    speechSynthesis.addEventListener('voiceschanged', populateVoices);
 }
